d8807c5fbfa8a379b3e390b9a903218e73c14db6
Muhammad Siddique <48084054+Muhammad1Siddique@users.noreply.github.com>
2021-04-17 08:32:21 +0500

    update component background color

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import {Message} from './Message.js'
 
 function App() {
   let [count, setCount] = useState(1);
+  let [isMorning, setMorning] = useState(false);
   return (
-    <div>
+    <div className={`${isMorning ? 'daylight':'box'}`}>
+      <h2>Good {isMorning ? 'Morning' : 'Night'}</h2>
       <Message counter={count}/>
       <button onClick={()=>setCount(++count)}>Click to Increment</button>
+      <button onClick={()=>setMorning(!isMorning)}>Check Status</button>
     </div>
-  
-
   )
   }
 export default App;
